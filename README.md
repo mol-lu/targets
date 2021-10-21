96d19ae9ac1e2caede3ddc057089911c55de04dc
Cocoa Hoto <cocoa@hoto.us>
2021-10-21 16:42:24 +0900

    Add project files

---
+++ README.md
@@ -0,0 +1,2 @@
+## mol.lu - Shared domain for Blue Archive (KR) users
+![mol-lu-animated](https://raw.githubusercontent.com/mol-lu/targets/master/mol-lu-animated.png)
---
+++ zones.ts
@@ -0,0 +1,21 @@
+/** 
+ * zones.ts
+ * 
+ * To get own subdomain, insert a line below with format:
+ * 'subdomain': 'CNAME;example.com'
+ *  ^1           ^2    ^3
+ * 1: Subdomain without root domain part (Lowercase)
+ * 2: Type of record, CNAME only supported (Uppercase)
+ * 3: Value of record, write full fdqn (Lowercase)
+ * 
+ * If you want cloudflare proxy, just add ';PROXY' at end of value like below:
+ * 'subdomain': 'CNAME;example.com;PROXY'
+ * 
+ * Some subdomains are NOT allowed for prevent abuse. For example, 'admin', 'about' are not accepted.
+ */ 
+
+
+export default {
+	'': 'CNAME;mol-lu.github.io',
+	'www': 'CNAME;mol-lu.github.io;PROXY'
+}
